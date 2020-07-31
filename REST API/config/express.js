4fda4a3576a3c776559dfd3218c9db6dae07cc91
glvchv <glavchev.vld@gmail.com>
2020-07-31 18:23:36 +0300

    added cors

--- REST API/config/express.js
+++ REST API/config/express.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 module.exports = (app) => {
+    app.use(cors());
     app.use(express.urlencoded({
         extended: true
     }));
