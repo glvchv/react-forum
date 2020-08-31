cb40b5abcfd44e59f20205d0afbc32913b88b8dd
glvchv <glavchev.vld@gmail.com>
2020-08-31 23:41:39 +0300

    added dotenv

--- REST API/config/config.js
+++ REST API/config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     port: process.env.PORT || 175,
-    privateKey: 'superSecretKey',
-    dbUrl: 'mongodb+srv://forumAdmin:superSecretPassword@forum.x6hwn.mongodb.net/forum?retryWrites=true&w=majority',
+    privateKey: process.env.PRIVATE_KEY,
+    dbUrl: `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@forum.x6hwn.mongodb.net/forum?retryWrites=true&w=majority`,
     cookieKey: 'x-auth-token'
 }
